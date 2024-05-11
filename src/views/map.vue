177144fa4cc36461a639886c125f3979f5fb91e3
mxxxOrz12 <402674230ma@gmail.com>
2024-05-11 17:01:43 +0800

    ma0511

--- src/views/map.vue
+++ src/views/map.vue
@@ -5,9 +5,10 @@
         label: 'label',
     }
     import axios from 'axios'
-    import { ref, onMounted } from 'vue'
+    import { ref, onMounted, watch, nextTick } from 'vue'
 
     import cgcs2000ToBaidu from '../assets/js/transform.js'
+    import * as echarts from 'echarts';
 
 
     const features = ref([]);
@@ -45,8 +46,103 @@
         console.log(info.value.feature);
 
     }
-
-
+    const drawer = ref(false);
+    const initChart = () => {
+        var chartDom = document.getElementById('map-info-left');
+        console.log(chartDom);
+        if (chartDom) {
+            var myChart = echarts.init(chartDom);
+            const option = {
+                title: {
+                    text: '大类统计图',
+                    left: 'center'
+                },
+                tooltip: {
+                    trigger: 'item'
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left'
+                },
+                series: [
+                    {
+                        name: 'Access From',
+                        type: 'pie',
+                        radius: '50%',
+                        data: [
+                            { value: 495, name: '公共设施' },
+                            { value: 6, name: '房屋土地' },
+                            { value: 7, name: '道路交通' },
+                            { value: 31, name: '园林绿化' },
+                            { value: 27, name: '市容环境' },
+                            { value: 2, name: '其他' }
+                        ],
+                        emphasis: {
+                            itemStyle: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }
+                ]
+            };
+            myChart.setOption(option);
+        };
+    }
+    const initChart1 = () => {
+        var chartDom = document.getElementById('map-info-right');
+        console.log(chartDom);
+        if (chartDom) {
+            var myChart = echarts.init(chartDom);
+            const option = {
+                title: {
+                    text: '公共设施统计图',
+                    left: 'center'
+                },
+                tooltip: {
+                    trigger: 'item'
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left'
+                },
+                series: [
+                    {
+                        name: 'Access From',
+                        type: 'pie',
+                        radius: '50%',
+                        data: [
+                            { value: 139, name: '井盖' },
+                            { value: 45, name: '雨水箅子' },
+                            { value: 1, name: '信息亭' },
+                            { value: 8, name: '消防设施' },
+                            { value: 8, name: '立杆' },
+                            { value: 275, name: '路灯' },
+                            { value: 18, name: '监控电子眼' },
+                            { value: 1, name: '邮筒' }
+                        ],
+                        emphasis: {
+                            itemStyle: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }
+                ]
+            };
+            myChart.setOption(option);
+        };
+    }
+    watch(drawer, (newValue) => {
+        if (newValue) {
+            nextTick().then(() => {
+                initChart();
+                initChart1();
+            });
+        }
+    });
 
 </script>
 
@@ -58,6 +154,8 @@
                 <p>校园地图信息管理平台</p>
             </div>
             <div class="header-right">
+                <el-button type="primary" class="header-link-left" @click="drawer = true">
+                    统计图表 </el-button>
                 <el-button type="primary">
                     <router-link to="/" class="header-link">返回首页</router-link>
                 </el-button>
@@ -98,6 +196,15 @@
                 </BInfoWindow>
             </BMap>
         </div>
+
+
+        <el-drawer v-model="drawer" title="校园地图信息统计" direction="btt" size="50%">
+            <div class="map-info">
+                <div id="map-info-left"></div>
+                <div id="map-info-right"></div>
+            </div>
+
+        </el-drawer>
 
         <div class="map-on" style="max-height: 600px; overflow: auto;">
             <el-card style="width: 400px" shadow="always">
